d00108ff7974a7207fcfb36d2bff960c2ce57b2b
Kingsley Elijah <kcelijah@mail.com>
2021-08-02 08:31:59 +0300

    search keyword in file
    search keyword in file and append the keyword found on the file to another folder

--- myfirstscriptproject.sh
+++ myfirstscriptproject.sh
@@ -1,12 +1,28 @@
 #! /bin/bash
 
+ mkdir ./hot-folder
+
 for myfile in *;
 do
 	if [ -f "$myfile" ]; then
-	echo "$myfile"
-	grep -ni "spo" "$myfile"
+
+		#echo "$myfile"
+
+		#grep -ni "spo" "$myfile"
+		check=$(grep -ni "spo" "$myfile")
+		if [ -z "$check" ]; then
+			echo "EMPTY"
+		else
+			echo "HOT FILE !!! FOUND!"
+			cp "$myfile" ./hot-folder
+			echo " " >> hot-folder/"$myfile"
+			echo "************" >> hot-folder/"$myfile"
+			echo "$check" >> hot-folder/"$myfile"
+		fi
 	else
 		echo "$myfile is NOT a file... "
 		echo " "
 	fi
+echo "-------------------------------------------------"
+
 done 
